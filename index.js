50d5f784c510897c12abc1eac35e0b6ba1c4b5ef
febriadj <iamfebriadji@gmail.com>
2021-08-29 21:55:59 +0700

    update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const routes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use('/api', routes);
 
 app.listen(port);
