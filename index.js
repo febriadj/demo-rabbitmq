cea2865f2a16060d29d10c65e56e4a08ee7f4745
febriadj <iamfebriadji@gmail.com>
2021-08-29 20:10:10 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require('express');
-const amqp = require('amqplib');
+const routes = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const amqpConn = async () => {
-  try {
-    const conn = await amqp.connect('amqp://localhost');
-    return conn;
-  }
-  catch (err) {
-    console.log(JSON.stringify(err, null, 2));
-    return process.exit(1);
-  }
-}
+app.use('/api', routes);
 
 app.listen(port);
 console.log(`Server running on Port ${port}`);
